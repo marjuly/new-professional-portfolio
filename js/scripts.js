9dfb5d8a3b1a9805002a84cd5228aed25fdd8b74
marjuly <56303827+marjuly@users.noreply.github.com>
2021-03-24 09:33:21 -0400

    commit i forgot to commit changes yesterday but there is always today

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ function scrollWin1() {
   window.scrollTo(0, 0);
 }
 function scrollWin2() {
-  window.scrollTo(0, 740);
+  window.scrollTo(0, 818);
 }
 function scrollWin3() {
-  window.scrollTo(0, 1320);
+  window.scrollTo(0, 1420);
 }
 function ScrollToBottom() {
   window.scrollTo(0, document.body.scrollHeight);
