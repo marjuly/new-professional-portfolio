3e22642c5742c02d998ffc724244ae944fff3d77
marjuly <56303827+marjuly@users.noreply.github.com>
2021-05-19 09:12:53 -0400

    update 5/19/21

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function scrollWin2() {
   window.scrollTo(0, 818);
 }
 function scrollWin3() {
-  window.scrollTo(0, 1420);
+  window.scrollTo(0, 1560);
 }
 function ScrollToBottom() {
   window.scrollTo(0, document.body.scrollHeight);
